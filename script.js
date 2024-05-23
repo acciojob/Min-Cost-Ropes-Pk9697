951a2bbc0488591b7b6b2f7b189071c3f83cc7b5
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-23 13:32:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	if(!arr || arr.length<=1) return 0;
+  arr.sort((a,b)=>a-b)
+
+	let cost=0;
+	let prefix=arr[0];
+	for(let i=1;i<arr.length;++i){
+		cost+=prefix+arr[i];
+		prefix+=arr[i];
+	}
+
+	return cost
 }
+
+// alert(mincost([1, 2, 3, 4, 5]))
 
 module.exports=mincost;
